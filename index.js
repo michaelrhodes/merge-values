60c0d9ee9dd21af591cbcbe37a5737aff76dffa1
michaelrhodes <spam@michaelrhod.es>
2018-11-20 14:06:33 +1100

    Use strict comparison operator

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function merge (v1, v2) {
   if (v1 === v2) return v1
 
   // Replace non-objects
-  if (t1 != 'object' && t2 != 'object') return v1 = v2, v1
+  if (t1 !== 'object' && t2 !== 'object') return v1 = v2, v1
 
   // Concatenate two arrays together
   if (arr(v1) && arr(v2)) return v1 = v1.concat(v2), v1
@@ -19,7 +19,7 @@ function merge (v1, v2) {
   if (arr(v1)) return v1[v1.length] = v2, v1
 
   // Make an array out of a value
-  if (t1 != 'object' || arr(v2)) return v1 = [v1].concat(v2), v1
+  if (t1 !== 'object' || arr(v2)) return v1 = [v1].concat(v2), v1
 
   // Merge one object into another
   var k, v1kv, v2kv
